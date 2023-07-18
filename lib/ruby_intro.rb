f874a86ccb899707f260bda58804cdd864d6f25b
JedsadaSrijunpoe <tae456852@gmail.com>
2023-07-18 20:18:19 +0700

    finished homework

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,51 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  arr.max(2).sum
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  arr.combination(2).each { |two_int|
+    return true if two_int.sum == n
+  }
+  false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  not_consonant = ["A", "E", "I", "O", "U"]
+  return false if s.empty?
+  return false unless s[0].match?(/[[:alpha:]]/)
+  return true unless not_consonant.include? s[0].upcase
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return false if s.empty?
+  s.each_char { |char| return false if char.match?(/[^01]/)}
+  return true if s.to_i(2) % 4 == 0
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn, price)
+    raise ArgumentError if isbn.empty? || price <= 0
+    @isbn = isbn
+    @price = price
+  end
+
+  attr_accessor :isbn, :price
+
+  def price_as_string
+    return "$%0.2f" % [price]
+  end
 end
